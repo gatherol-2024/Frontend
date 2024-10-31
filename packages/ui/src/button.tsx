7d41e660bb1190a7c66b596d406c43b3dcf50a0e
dohi0512 <donghuni512@gmail.com>
2024-10-31 15:24:14 +0900

    Revert "Init"
    This reverts commit 90ae49b7edb9b45c2457d855c3ee13aabeb7bc3c.

--- packages/ui/src/button.tsx
+++
@@ -1,5 +0,0 @@
-const Button = () => {
-  return <div className="bg-test text-2xl">dWDHIHI</div>
-}
-
-export default Button
--- packages/ui/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from 'tailwindcss'
-import sharedConfig from '@lvc/config-tailwind'
-const config: Pick<Config, 'content' | 'presets' | 'prefix'> = {
-  presets: [sharedConfig],
-  content: ['./src/**/*.tsx', './src/*.tsx']
-}
-export default config
