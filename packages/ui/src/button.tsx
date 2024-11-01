81f96dbc1fa73f3d4b7a94e8933227e9c60d6bcb
dohi0512 <donghuni512@gmail.com>
2024-11-01 13:29:40 +0900

    Feat:: Button 컴포넌트

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,5 +1,22 @@
-const Button = () => {
-  return <div className="bg-test text-xl">dWDHIHI</div>
+import { type ButtonProps } from '@lvc/shared/types'
+const Button = ({
+  size = 'md',
+  children,
+  className,
+  ...props
+}: ButtonProps) => {
+  const SIZE: Record<typeof size, string> = {
+    xs: 'px-2 py-1 text-xs font-medium',
+    sm: 'px-3 py-1.5 text-sm font-medium',
+    md: 'px-4 py-2 text-base font-semibold',
+    lg: 'px-5 py-3 text-lg font-semibold',
+    xl: 'px-6 py-4 text-xl font-bold'
+  }
+  return (
+    <button className={`${className} ${SIZE[size]} outline-none`} {...props}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
