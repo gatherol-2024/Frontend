70bc1fa4508664d04aa022c1f871088e55101714
dohi0512 <donghuni512@gmail.com>
2024-11-01 13:29:03 +0900

    Style:: 포맷팅 변경

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,15 +1,15 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions'
   ],
   framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-};
-export default config;
+    name: '@storybook/nextjs',
+    options: {}
+  }
+}
+export default config
--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,13 +1,14 @@
 /** @type { import('@storybook/react').Preview } */
+import '../src/styles.css'
 const preview = {
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-};
+        date: /Date$/i
+      }
+    }
+  }
+}
 
-export default preview;
+export default preview
