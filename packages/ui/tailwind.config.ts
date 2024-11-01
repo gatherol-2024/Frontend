be21fbd7475a1d60715b9917e3cbd34b53dc922b
dohi0512 <donghuni512@gmail.com>
2024-11-01 13:29:20 +0900

    Feat:: storybook 추가

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,6 +2,6 @@ import type { Config } from 'tailwindcss'
 import sharedConfig from '@lvc/config-tailwind'
 const config: Pick<Config, 'content' | 'presets' | 'prefix'> = {
   presets: [sharedConfig],
-  content: ['./src/**/*.tsx', './src/*.tsx']
+  content: ['./src/**/*.{tsx,ts}', './src/*.{tsx,ts}']
 }
 export default config
