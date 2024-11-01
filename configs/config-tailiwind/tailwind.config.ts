fd416d5112f41813b1207addb102d76e1ed9f285
dohi0512 <donghuni512@gmail.com>
2024-11-01 23:41:00 +0900

    Feat:: custom color 추가

--- configs/config-tailiwind/tailwind.config.ts
+++ configs/config-tailiwind/tailwind.config.ts
@@ -1,17 +1,35 @@
-import { Config } from 'tailwindcss'
+import { Config } from 'tailwindcss';
 
 const preset: Omit<Config, 'content'> = {
   theme: {
-    colors: {
-      primary: '#029CFD',
-      secondary: '#FF7F32',
-      white: '#FFF',
-      black: '#000'
-    },
-    screens: {
-      xs: '480px'
+    extend: {
+      colors: {
+        primary: {
+          50: '#edfcff',
+          100: '#d6f7ff',
+          200: '#b6f4ff',
+          300: '#84efff',
+          500: '#20c9ff',
+          600: '#08afff',
+          DEFAULT: '#029cfd',
+          800: '#0977c4',
+          900: '#0e639a'
+        },
+        secondary: {
+          50: '#fff6ed',
+          100: '#ffead4',
+          200: '#ffd1a8',
+          300: '#ffaf71',
+          DEFAULT: '#ff7f32',
+          500: '#fe6011',
+          600: '#ef4607',
+          700: '#c63108',
+          800: '#9d280f',
+          900: '#7e2410'
+        }
+      }
     }
   }
-}
+};
 
-export default preset
+export default preset;
