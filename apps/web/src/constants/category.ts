ce32be401c8167b780b60447fef5a799bcf54a3e
dohi0512 <donghuni512@gmail.com>
2024-11-04 23:35:20 +0900

    Feat:: 카테고리 상수 추가

---
+++ apps/web/src/constants/category.ts
@@ -0,0 +1,14 @@
+export const CATEGORY = [
+  {
+    itemTitle: '전체',
+    itemValue: 'ALL'
+  },
+  {
+    itemTitle: '팀 구하기',
+    itemValue: 'TEAM_FINDING'
+  },
+  {
+    itemTitle: '잡담',
+    itemValue: 'CASUAL_TALK'
+  }
+];
