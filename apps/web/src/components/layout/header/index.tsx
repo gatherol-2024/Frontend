2e423908604494dbb81d52a638195dc40f29e9a2
dohi0512 <donghuni512@gmail.com>
2024-11-04 23:30:34 +0900

    Fix:: 링크 이상하던 문제 수정

--- apps/web/src/components/layout/header/index.tsx
+++ apps/web/src/components/layout/header/index.tsx
@@ -1,18 +1,18 @@
-import { Button } from '@lvc/ui'
-import HeaderMenu from './menu'
+import { Button } from '@lvc/ui';
+import HeaderMenu from './menu';
 
 const Header = () => {
   return (
-    <nav className="fixed flex h-16 w-full items-center px-8">
+    <nav className="flex h-16 w-full items-center bg-white px-8">
       <div className="text-2xl font-bold">LOGO</div>
       <div className="flex flex-1 justify-center gap-4">
-        <HeaderMenu href="/">커뮤니티</HeaderMenu>
-        <HeaderMenu href="test1">팀보이스</HeaderMenu>
-        <HeaderMenu href="test2">보이스챗</HeaderMenu>
+        <HeaderMenu href="/community">커뮤니티</HeaderMenu>
+        <HeaderMenu href="/teamvoice">팀보이스</HeaderMenu>
+        <HeaderMenu href="/voicechat">보이스챗</HeaderMenu>
       </div>
       <Button>로그인</Button>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
