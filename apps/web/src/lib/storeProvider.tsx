7c451da39ed4e89a66802f694b11f9d643f70f17
dohi0512 <donghuni512@gmail.com>
2024-11-05 23:11:01 +0900

    Feat:: redux ssr provider

---
+++ apps/web/src/lib/storeProvider.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { AppStore, makeStore } from '@lvc/shared/store';
+import { ReactNode, useRef } from 'react';
+import { Provider } from 'react-redux';
+
+const StoreProvider = ({ children }: { children: ReactNode }) => {
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+};
+
+export default StoreProvider;
